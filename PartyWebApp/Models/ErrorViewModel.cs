af18626871031bfc47b9bd4c0ebebee83ef19755
Raz Elimelech <razelimel@gmail.com>
2021-04-07 20:16:35 +0300

    Add project files.

---
+++ PartyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PartyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
