9ed73802cf53431ca7d4ae87dec7cbda197cd1da
Raz Elimelech <razelimel@gmail.com>
2021-04-16 09:47:57 +0300

    created all of the models currently needed in the project.

---
+++ PartyWebApp/Models/Party.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyWebApp.Models
+{
+    public class Party
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime eventDate { get; set; }
+        public Genre genre { get; set; }
+        public int minimalAge { get; set; }
+        public Performer[] performers { get; set; }
+        public Attendee[] attendees { get; set; }
+        public Club club { get; set; }
+    }
+}
---
+++ PartyWebApp/Models/Performer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyWebApp.Models
+{
+    public class Performer
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public Genre genre { get; set; }
+        public double rating { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
