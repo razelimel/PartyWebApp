da17633f0705ea2af5987bfaa11242cb419ec892
Raz Elimelech <razelimel@gmail.com>
2021-04-20 14:19:07 +0300

    fix/db-connection

--- PartyWebApp/Startup.cs
+++ PartyWebApp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PartyWebApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace PartyWebApp
 {
@@ -24,6 +26,9 @@ namespace PartyWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<PartyWebAppContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("PartyWebAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
